f946abc829e1fb7e74e278c4e095929ee551672b
Alim1899 <alimakhchanogli@gmail.com>
2024-06-24 14:47:49 +0400

    Made animation responsive

--- src/components/First/First.js
+++ src/components/First/First.js
@@ -1,14 +1,15 @@
 import classes from "./First.module.css";
 import aa from "../../assets/aa.svg";
+
 const First = () => {
   return (
     <div className={classes.main}>
       <div className={classes.wrapper}>
         <div className={classes.content}>
-          <img src={aa} className={classes.svg} alt="aa"></img>
+          <img src={aa} className={classes.svg} alt="aa" />
           <div className={classes.text}>
             <h2>
-              Hey, i'm <strong>Alim</strong>
+              Hey, I'm <strong>Alim</strong>
             </h2>
             <h2>
               I'm a Front-End <strong>Web-Developer</strong>
@@ -16,15 +17,20 @@ const First = () => {
             <h2>Based in Georgia, Tbilisi</h2>
           </div>
         </div>
-        <div className={classes.content }>
-        <div className={classes.content}>
-            <div className={classes.bigCircle}>
-                <div className={classes.midCircle}>
-                    <div className={classes.smallCircle}></div>
-                </div>
+        <div className={classes.animation}>
+          <div className={classes.bigCircle}>
+            <div className={classes.midCircle}>
+              <div className={classes.smallCircle}></div>
             </div>
+          </div>
+          <div className={classes.lines}>
+            <div className={classes.line}></div>
+            <div className={classes.line}></div>
+            <div className={classes.line}></div>
+            <div className={classes.line}></div>
+            <div className={classes.line}></div>
+          </div>
         </div>
-    </div>
       </div>
     </div>
   );
