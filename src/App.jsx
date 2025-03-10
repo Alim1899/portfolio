114ca9200fa89618666aa3df36714c79f83a98e0
Alim1899 <alimakhchanogli@gmail.com>
2025-03-10 20:33:05 +0400

    Styled navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
+
 function App() {
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      document.documentElement.style.setProperty("--x", `${e.clientX}px`);
+      document.documentElement.style.setProperty("--y", `${e.clientY}px`);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
-    <>
-      <Layout />{" "}
-    </>
+    <div className="hover">
+      <Layout />
+    </div>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,33 +3,32 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import classes from "./Navbar.module.css";
+import { useState } from "react";
 const Navbar = () => {
+  const [selected, setSelected] = useState("About");
+
   return (
     <div className={classes.navbar}>
       <div className={classes.personal}>
-        <h1>Alim Akhchanogli</h1>
-        <h3>Front End Developer</h3>
-        <h6>
+        <h1 className={classes.name}>Alim Akhchanogli</h1>
+        <h3 className={classes.occupation}>Front End Developer</h3>
+        <h4 className={classes.description}>
           I build accessible, pixel-perfect digital experiences for the web.
-        </h6>
+        </h4>
       </div>
       <div className={classes.navlinks}>
-        <div className={classes.link}>
-          <hr />
-          About
-        </div>
-        <div className={classes.link}>
-          <hr />
-          Experience
-        </div>
-        <div className={classes.link}>
-          <hr />
-          Skillset
-        </div>
-        <div className={classes.link}>
-          <hr />
-          Projects
-        </div>
+        {["About", "Experience", "Skillset", "Projects"].map((item) => (
+          <button
+            key={item}
+            className={`${classes.link} ${
+              selected === item ? classes.selected : ""
+            }`}
+            onClick={() => setSelected(item)}
+          >
+            <hr />
+            {item}
+          </button>
+        ))}
       </div>
       <div className={classes.social}>
         <GrGithub />
